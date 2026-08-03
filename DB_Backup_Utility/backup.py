40dbf6cf6c6248496b768b5e8427099df407fbc1
xsuwittch <ammarworksonline@gmail.com>
2026-08-03 09:39:10 -0700

    Differnt db handling

--- DB_Backup_Utility/backup.py
+++ DB_Backup_Utility/backup.py
@@ -13,12 +13,24 @@ print(r"""
 
 """)
 import subprocess
+import shutil
 
 def backup(username, password, db_type, db_name, backup_file):
     try:
-        if db_type == "mysql":
-            command = ["mysqldump", f"-u {username}", f"-p{password}", db_name] # used a list to avoid using shell=True
-            with open(backup_file) as f:
-                subprocess.STDOUT = f
-                subprocess.run(command, check=True) # check = true so that first except can get triggered
+        match db_type:
+            case "mysql":
+                command = ["mysqldump", f"-u{username}", f"-p{password}", db_name] # used a list to avoid using shell=True
+            case "postgresql":
+                command = ["pg_dump", "-U", username, "-F", "c", "-b", "-v", "-f", backup_file, db_name]
+            case "mongodb":
+                command = ["mongodump", "--db", db_name, "--out", backup_file]
+            case "sqlite":
+                shutil(db_name,backup_file)
+        with open (backup_file, w) as file:
+            subprocess.STDOUT = file
+            subprocess.run(command, check=True)
+    except subprocess.CalledProcessError:
+        print(" Error Occured during backup")
+    return backup_file
+
                 
