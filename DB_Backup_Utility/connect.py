b21efd1cdd9e0f5558240238d2e804ea3b63a130
xsuwittch <ammarworksonline@gmail.com>
2026-08-04 04:53:56 -0700

    fixed bugs in connect.py

--- DB_Backup_Utility/connect.py
+++ DB_Backup_Utility/connect.py
@@ -2,16 +2,25 @@ import mysql.connector
 import psycopg2
 import pymongo
 import sqlite3
-def connect_db(username, inp_password,hostname, db , db_type, port):
-    match db_type: 
+
+def connect_db(username, inp_password, hostname, db, db_type, port):
+    match db_type:
         case "mysql":
-            connection = mysql.connector.connect(host = hostname, user = username, password = inp_password, database = db)
+            connection = mysql.connector.connect(
+                host=hostname, port=port, user=username,
+                password=inp_password, database=db
+            )
         case "postgresql":
-            connection = psycopg2.connect(host = hostname, user=username, password=inp_password, dbname=db)
+            connection = psycopg2.connect(
+                host=hostname, port=port, user=username,
+                password=inp_password, dbname=db
+            )
         case "mongodb":
-            connectioln= pymongo.MongoClient(f"mongodb://{hostname}:{port}")
+            connection = pymongo.MongoClient(
+                f"mongodb://{username}:{inp_password}@{hostname}:{port}"
+            )
         case "sqlite3":
             connection = sqlite3.connect(db)
 
-    print(" Connected ")
+    print("Connected")
     return connection
